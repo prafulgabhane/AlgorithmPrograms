a116f458818f6a247a25dddcd3f1a67c8075bef6
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-13 18:04:06 +0530

    Create Binary.cs

---
+++ Binary.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class Program
+    {
+      public static object BinarySearchDisplay(int[] arr, int key) {
+         int minNum = 0;
+         int maxNum = arr.Length - 1;
+
+         while (minNum <=maxNum) {
+            int mid = (minNum + maxNum) / 2;
+            if (key == arr[mid]) {
+               return ++mid;
+            } else if (key < arr[mid]) {
+               max = mid - 1;
+            }else {
+               min = mid + 1;
+            }
+         }
+         return "None";
+      }
+    }
