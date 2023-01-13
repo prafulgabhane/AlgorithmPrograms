2c2d4f216b7ea44d28112d10377c63ef13cf2a41
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-13 17:57:48 +0530

    Update StringPermutations.cs

--- StringPermutations.cs
+++ StringPermutations.cs
@@ -1,53 +1,18 @@
 // C# program to print all
 // permutations of a given string.
+
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-class GFG {
-	/**
-	* permutation function
-	* @param str string to
-	calculate permutation for
-	* @param l starting index
-	* @param r end index
-	*/
-	private static void permute(String str, int l, int r)
-	{
-		if (l == r)
-			Console.WriteLine(str);
-		else {
-			for (int i = l; i <= r; i++) {
-				str = swap(str, l, i);
-				permute(str, l + 1, r);
-				str = swap(str, l, i);
-			}
-		}
-	}
+namespace AlgorithmPrograms
+{
+    public class AlgorithmPrograms
 
-	/**
-	* Swap Characters at position
-	* @param a string value
-	* @param i position 1
-	* @param j position 2
-	* @return swapped string
-	*/
-	public static String swap(String a, int i, int j)
-	{
-		char temp;
-		char[] charArray = a.ToCharArray();
-		temp = charArray[i];
-		charArray[i] = charArray[j];
-		charArray[j] = temp;
-		string s = new string(charArray);
-		return s;
-	}
-
-	// Driver Code
-	public static void Main()
-	{
-		String str = "ABC";
-		int n = str.Length;
-		permute(str, 0, n - 1);
-	}
+class StringPermutations 
+{
+	
 }
-
-// This code is contributed by mits
