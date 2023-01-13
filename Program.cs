943d52338ef643fb1a590181cd03a40f74567d9b
Praful Gabhane <45679855+prafulgabhane@users.noreply.github.com>
2023-01-13 18:05:11 +0530

    Update Program.cs

---
+++ Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome Algorithm Program");
+            
+            StringPremutations.ListPeople();
+            Console.ReadLine();
+
+        }
+    }
+}
